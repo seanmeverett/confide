43328135d473914852a8ad3910fb395ae6a207eb
Sean Everett <seanmeverett@gmail.com>
2012-11-18 10:23:56 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Confide::Application.config.secret_token = 'c0a41a5936e8c7124584f15419f6006e9008245192e15b1206c39223c1d3284e196e42c618e76670f8c5e6bf76f3baf70b5039c681dc4423486d6d693e1f0939'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Confide::Application.config.session_store :cookie_store, key: '_confide_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Confide::Application.config.session_store :active_record_store
